c416469d68e1ca3751baff6df608ab1a221f9eb5
Musab36 <musabhd@gmail.com>
2017-10-30 18:27:35 +0300

    Initial commit

---
+++ app/src/main/java/com/sandule/mexercises/ListScientistsFragment.java
@@ -0,0 +1,30 @@
+package com.sandule.mexercises;
+
+import android.support.v4.app.Fragment;
+
+
+public class ListScientistsFragment extends Fragment {
+    /*
+    private String[] scientistNames = {"Marie Curie","Thomas Edison","Albert Einstein","Michael Faraday","Galileo Galilei",
+            "Stephen Hawking","Johannes Kepler","Issac Newton","Nikola Tesla"};
+    private String[] birthYear = {"1867","1847","1879","1791","1564","1942","1571","1643","1856"};
+    private String[] deathYear = {"1934","1931","1955","1867","1642","Present","1630","1727","1943"};
+    private int[] image = {R.drawable.doctor, R.drawable.doctor, R.drawable.doctor, R.drawable.doctor, R.drawable.doctor,
+            R.drawable.doctor, R.drawable.doctor, R.drawable.doctor, R.drawable.doctor};
+    public ArrayList<Scientist> mScientists;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mScientists = new ArrayList<>();
+        for(int i = 0; i < scientistNames.length; i++) {
+            Scientist s = new Scientist();
+            s.setName(scientistNames[i]);
+            s.setBirthYear(birthYear[i]);
+            s.setDeathYear(deathYear[i]);
+            s.setImageId(image[i]);
+            mScientists.add(s);
+        }
+        super.onCreate(savedInstanceState);
+    }
+    */
+}
---
+++ app/src/main/java/com/sandule/mexercises/MainActivity.java
@@ -0,0 +1,156 @@
+package com.sandule.mexercises;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    Button mSendBtn;
+    Button mFectBtn;
+    @Bind(R.id.sendEditText) EditText mSendEditText;
+    @Bind(R.id.fetchTextView) TextView mFecthTextView;
+    DatabaseReference rootRef, quizRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+        //Binding buttons
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mSendBtn = (Button) findViewById(R.id.sendBtn);
+        mFectBtn = (Button) findViewById(R.id.fetchBtn);
+
+        //Firebase setup
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        quizRef = rootRef.child("questions");
+
+        //OnClickListeners
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String question = mSendEditText.getText().toString();
+                quizRef.push().setValue(question);
+            }
+        });
+
+        mFectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quizRef.child("question").addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String question = dataSnapshot.getValue(String.class);
+                        mFecthTextView.setText(question);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
